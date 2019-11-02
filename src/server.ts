69d4ef68ecf63e503539d5139878848923aa720b
Wojciech Dudek <wdudek82@gmail.com>
2019-11-02 17:11:46 +0100

    Change project structure

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,16 @@
-import dotenv from "dotenv";
-import path from "path";
 import express, { Express } from "express";
 import * as http from "http";
 import { applyMiddleware, applyRoutes } from "./util";
 import middleware from "./middleware";
-import routes from "./routes/index";
+import routes from "./services";
+import dotenv from "dotenv";
 
-const DEBUG = true;
-
-const ENV_FILE_PATH = `../.env.${DEBUG ? "development" : "production"}`;
-
-// initialize configuration
-dotenv.config({
-  path: path.resolve(__dirname, ENV_FILE_PATH),
-});
+dotenv.config();
 
 const { SERVER_PORT = 8000 } = process.env;
 
 const router: Express = express();
+
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
 
