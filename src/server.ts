d93557d30a1dab7f503ee76629fa0c3ca7be6f20
Wojciech Dudek <wdudek82@gmail.com>
2019-11-02 01:03:31 +0100

    Add additional filters, key dependencies to README, update package.json, refactor server.ts

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import path from "path";
+import express, { Express, NextFunction, Request, Response } from "express";
+import * as http from "http";
+
+const DEBUG = true;
+
+const ENV_FILE_PATH = `../.env.${DEBUG ? "development" : "production"}`;
+
+// initialize configuration
+dotenv.config({
+  path: path.resolve(__dirname, ENV_FILE_PATH),
+});
+
+const { SERVER_PORT = 8000 } = process.env;
+
+const router: Express = express();
+const server = http.createServer(router);
+
+// define a route handler for the default home page
+router.get("/", (req: Request, res: Response, next: NextFunction): void => {
+  res.send("<h1>Hello, World!:)</h1>");
+});
+
+server.listen(SERVER_PORT, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Server started at http://localhost:${SERVER_PORT}`);
+});
