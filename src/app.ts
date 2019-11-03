d26fe69b7929d860998e981aac4fe7aa2977743b
Wojciech Dudek <wdudek82@gmail.com>
2019-11-03 21:52:23 +0100

    Extract server setup and start to another function

--- src/app.ts
+++ src/app.ts
@@ -8,25 +8,29 @@ import dotenv from "dotenv";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
+function startServer() {
+  dotenv.config();
+
+  const { SERVER_PORT = 8000 } = process.env;
+
+  const app: Express = express();
+
+  applyMiddleware(middleware, app);
+  applyRoutes(routes, app);
+
+  const server: Server = http.createServer(app);
+
+  server.listen(SERVER_PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server started at http://localhost:${SERVER_PORT}`);
+  });
+}
+
 // create connection with database
 // note that it's not active database connection
 // TypeORM creates connection pools and uses them for your requests
 createConnection()
   .then(async () => {
-    dotenv.config();
-
-    const { SERVER_PORT = 8000 } = process.env;
-
-    const app: Express = express();
-
-    applyMiddleware(middleware, app);
-    applyRoutes(routes, app);
-
-    const server: Server = http.createServer(app);
-
-    server.listen(SERVER_PORT, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Server started at http://localhost:${SERVER_PORT}`);
-    });
+    startServer();
   })
   .catch((error) => console.log("TypeORM connection error:", error));
--- src/middleware/common.ts
+++ src/middleware/common.ts
@@ -7,7 +7,7 @@ export const handleCors = (router: Router) => {
   router.use(
     cors({
       origin: "*",
-      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "HEAD", "OPTIONS"],
       preflightContinue: false,
       credentials: true,
       optionsSuccessStatus: 204,
